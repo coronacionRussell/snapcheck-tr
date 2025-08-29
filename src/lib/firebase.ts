12776830c31032b250fb37ab0a146a367fc60a87
RUSSELL CORONACION <russellcoronacion1219@gmail.com>
2025-08-29 21:09:04 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -17,6 +17,5 @@ const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
 const db = getFirestore(app);
 const auth = getAuth(app);
 auth.tenantId = null;
-auth.settings.authorizedDomains.push("127.0.0.1");
 
 export { app, db, auth };
