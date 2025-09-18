425aae2b222181996ec36e62a691b9083f55cc3b
RUSSELL CORONACION <russellcoronacion1219@gmail.com>
2025-09-18 15:12:24 +0000

    can you remove all the changes from the last 4 prompts

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -36,16 +36,7 @@ export default function RootLayout({
         <link
           href="https://fonts.googleapis.com/css2?family=PT+Sans:ital,wght@0,400;0,700;1,400;1,700&family=Source+Code+Pro:wght@400;700&display=swap"
           rel="stylesheet"
-          media="print"
-          // @ts-ignore
-          onLoad="this.media='all'"
         />
-        <noscript>
-           <link
-              href="https://fonts.googleapis.com/css2?family=PT+Sans:ital,wght@0,400;0,700;1,400;1,700&family=Source+Code+Pro:wght@400;700&display=swap"
-              rel="stylesheet"
-            />
-        </noscript>
       </head>
       <body className="font-body antialiased">
         {children}
