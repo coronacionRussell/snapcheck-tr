32c4a2a1e61e50dd86a81e3913e91afb9a1b52a9
RUSSELL CORONACION <russellcoronacion1219@gmail.com>
2025-09-23 11:21:41 +0000

    Try fixing this error: `Runtime Error: Error: Invalid src prop (https://

--- next.config.ts
+++ next.config.ts
@@ -50,6 +50,12 @@ const nextConfig: NextConfig = {
         hostname: 'i.imgur.com',
         port: '',
         pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'firebasestorage.googleapis.com',
+        port: '',
+        pathname: '/**',
       }
     ],
   },
