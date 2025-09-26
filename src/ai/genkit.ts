25f7cb41838070bb696c3ab774827a358166301a
RUSSELL CORONACION <russellcoronacion1219@gmail.com>
2025-09-26 03:38:07 +0000

    Try fixing this error: `Console Error: Error: [GoogleGenerativeAI Error]

--- src/ai/genkit.ts
+++ src/ai/genkit.ts
@@ -1,8 +1,8 @@
 
 import {genkit} from 'genkit';
-import {googleAI} from '@genkit-ai/googleai';
+import {vertexAI} from '@genkit-ai/vertexai';
 
 export const ai = genkit({
-  plugins: [googleAI()],
-  model: 'googleai/gemini-pro',
+  plugins: [vertexAI()],
+  model: 'vertexai/gemini-1.5-flash-latest',
 });
